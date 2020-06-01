a85b12ac97d6dd6b4bd0a1f3260f5954b93c85b6
Anishaa De Silva <57263847+anishaa95@users.noreply.github.com>
2020-06-01 18:35:59 -0400

    updated app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import json
 #################################################
 
 engine = create_engine(
-    "postgres://marciooliver:@localhost:5432/HappinessData")
+    "postgres://AnishaaDeSilva:@localhost:5432/HappinessData")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -24,6 +24,7 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 # print(Base.classes)
 Happiness = Base.classes.happiness
+Happiness_Report = Base.classes.happiness_report
 
 #################################################
 # Flask Setup
@@ -43,22 +44,32 @@ def index():
     return render_template("index.html", data=data)
 
 
-# Jsonify data route
+# Jsonify happiness data route
 @app.route("/api/v1.0/happyness_index")
 def happyness_index():
     # Get all data from DB
     data = engine.execute("SELECT * FROM happiness")
+    
     # jsonify data to render template
     return jsonify({'data': [dict(row) for row in data]})
+
+
+# Render Happiness Report data to html template
+@app.route("/project3")
+def project3():
+    ML_data = engine.execute("SELECT * FROM happiness_report")
+    return render_template("project3.html", ML_data=ML_data)
+
+
+# Jsonify data route for happiness report data
+@app.route("/api/v1.0/happyness_report_data")
+def happyness_report_data():
+    # Get all data from DB
+    ML_data = engine.execute("SELECT * FROM happiness_report")
+
+    # jsonify data to render template
+    return jsonify({'ML_data': [dict(row) for row in ML_data]})
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-# Code from Reed
-# @app.route('/output',methods=["POST"])
-# def get_output():
-#     json = request.get_json()
-#     input_data = [[json["data"]['S1'],json["data"]['S2'],json["data"]['S3'],[json["data"]['F1'],json["data"]['F2'],json["data"]['C1'],[json["data"]['C2'],json["data"]['C3'],json["data"]['P1'],json["data"]['P2']]]
-
-#     return "marcio"
